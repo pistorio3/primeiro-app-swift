e0c9d6a9d9471e9db46ff4144468087fe1ad9f72
João Victor Pistório <joao.pistorio@gmail.com>
2022-11-21 19:10:02 -0300

    Criando projeto

---
+++ PrimeiroApp/PrimeiroApp/PrimeiroAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  PrimeiroAppApp.swift
+//  PrimeiroApp
+//
+//  Created by Joao Victor Pistorio Martins on 21/11/22.
+//
+
+import SwiftUI
+
+@main
+struct PrimeiroAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
